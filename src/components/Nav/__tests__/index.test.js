f6a289b0e721f268b89a6a79fe696af074b1ba88
Your Name <you@example.com>
2021-09-19 23:22:25 -0500

    corrected testing

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import Nav from './components/Nav/Nav';
+import Nav from '../Nav';
 
 afterEach(cleanup);
 
